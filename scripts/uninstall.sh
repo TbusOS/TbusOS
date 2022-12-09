fd4af7b787f04279b9ef0051822571dda1c9065a
Master1wx <571550728@qq.com>
2022-12-09 16:03:19 +0800

    1.Support uninstall.

---
+++ scripts/uninstall.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# uninstall TbusOS.
+#
+# Copyright (C) 2022.12.09 by liaowenxiong <571550728@qq.com>
+#
+# SPDX-License-Identifier: GPL-2.0
+
+uninstall_rootfs()
+{
+	rm -rf ${TbusOS}/TbusOS/rootfs
+}
+
+uninstall_qemu()
+{
+	rm -rf ${TbusOS}/TbusOS/qemu
+}
+
+uninstall_kernel()
+{
+	rm -rf ${TbusOS}/TbusOS/kernel
+}
+
+case $1 in
+	--rootfs)
+		uninstall_rootfs
+		;;
+	--qemu)
+		uninstall_qemu
+		;;
+    --kernel)
+        uninstall_kernel
+        ;;
+    --help | -h | *)
+        echo "[Usage] ./uninstall.sh"
+        echo "--rootfs	uninstall rootfs"
+        echo "--qemu		uninstall qemu"
+        echo "--kernel		uninstall kernel"
+        ;;
+esac
